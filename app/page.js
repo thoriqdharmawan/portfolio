ca2a820289ce80d4c07a448ed59d24d1f304a818
thoriqdharmawan <thoriqd21@gmail.com>
2023-06-12 22:23:24 +0700

    fix: typo

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 "use client"
 
 import Content from "@/components/shared/Content";
+import Darkmode from "@/components/ui/Darkmode";
 import Hero from "@/components/ui/Hero";
 import Projects from "@/components/ui/Projects";
 import Skills from "@/components/ui/Skills";
 import WhatCanBeDone from "@/components/ui/WhatCanBeDone";
-import Darkmode from "@/components/ui/darkmode";
 
 export default function Home() {
   return (
