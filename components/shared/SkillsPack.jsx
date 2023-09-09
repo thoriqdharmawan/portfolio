99a9c78145380e0738e62b0b69aa74ad7bde1c33
thoriqdharmawan <thoriqd21@gmail.com>
2023-09-09 23:17:43 +0700

    feat: update skills section

--- components/shared/SkillsPack.jsx
+++ components/shared/SkillsPack.jsx
@@ -4,21 +4,7 @@ import Image from "next/image";
 const SkillIcon = ({ src, name }) => {
   return (
     <Tooltip title={name}>
-      <Paper
-        elevation={0}
-        sx={{
-          width: "fit-content",
-          padding: "8px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          borderRadius: "8px",
-          position: "relative",
-          "&:hover": {
-            boxShadow: "rgba(145, 158, 171, 0.16) 0px 24px 48px 0px",
-          },
-        }}
-      >
+      <Paper elevation={0} sx={styles.skillIcon}>
         <Image src={src} width={50} height={50} alt={name} />
       </Paper>
     </Tooltip>
@@ -27,34 +13,54 @@ const SkillIcon = ({ src, name }) => {
 
 export default () => {
   return (
-    <Box
-      sx={{
-        p: 2,
-        backgroundColor: "#fcfcfc",
-        display: "flex",
-        flexDirectioin: "row",
-        flexWrap: 'wrap',
-        gap: 2,
-      }}
-    >
-      <SkillIcon src="/skills/ic_skill_react.svg" name="ReactJs" favorite />
+    <Box sx={styles.skillWrapper}>
+      <SkillIcon src="/skills/ic_skill_react.svg" name="ReactJs" />
       <SkillIcon src="/skills/ic_skill_js.svg" name="Javscript" />
       <SkillIcon src="/skills/ic_skill_ts.svg" name="Typescript" />
-      <SkillIcon src="/skills/ic_skill_next.svg" name="NextJs" favorite />
+      <SkillIcon src="/skills/ic_skill_next.svg" name="NextJs" />
       <SkillIcon src="/skills/ic_skill_redux.svg" name="Redux" />
       <SkillIcon src="/skills/ic_skill_vite.svg" name="Vite" />
       <SkillIcon src="/skills/ic_skill_mui.svg" name="MaterialUI" />
       <SkillIcon src="/skills/ic_skill_antd.svg" name="AntDesign" />
       <SkillIcon src="/skills/ic_skill_tailwind.svg" name="Tailwind" />
-      <SkillIcon src="/skills/ic_skill_styledcomponent.svg" name="StyledComponent" />
+      <SkillIcon
+        src="/skills/ic_skill_styledcomponent.svg"
+        name="StyledComponent"
+      />
       <SkillIcon src="/skills/ic_skill_react-router.svg" name="React Router" />
       <SkillIcon src="/skills/ic_skill_hasura.svg" name="Hasura" />
       <SkillIcon src="/skills/ic_skill_graphql.svg" name="Graphql" />
       <SkillIcon src="/skills/ic_skill_formik.svg" name="Formik" />
-      <SkillIcon src="/skills/ic_skill_react-hook-form.svg" name="React Hook Form" />
+      <SkillIcon
+        src="/skills/ic_skill_react-hook-form.svg"
+        name="React Hook Form"
+      />
       <SkillIcon src="/skills/ic_skill_express.svg" name="Express.js" />
       <SkillIcon src="/skills/ic_skill_figma.svg" name="Figma" />
       <SkillIcon src="/skills/ic_skill_sql.svg" name="SQL" />
     </Box>
   );
+};
+
+const styles = {
+  skillWrapper: {
+    p: 2,
+    backgroundColor: "#fcfcfc",
+    display: "flex",
+    flexDirectioin: "row",
+    flexWrap: "wrap",
+    gap: 2,
+  },
+  skillIcon: {
+    width: "fit-content",
+    padding: "8px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: "8px",
+    position: "relative",
+    "&:hover": {
+      boxShadow: "rgba(145, 158, 171, 0.16) 0px 24px 48px 0px",
+    },
+  },
 };
--- components/ui/Skills.jsx
+++ components/ui/Skills.jsx
@@ -6,22 +6,29 @@ import SkillsPack from "../shared/SkillsPack";
 export default () => {
   return (
     <Section>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <SkillsPack />
-        </Grid>
-        <Grid item xs={6} sx={{ display: "flex", alignItems: "center" }}>
-          <Box>
-            <Typography variant="h2" fontWeight="bold" mb={2}>
-              Huge pack of Skills
-            </Typography>
-
-            <Button component={Link} href="/skills" variant="contained">
-              Show All Skills
-            </Button>
-          </Box>
-        </Grid>
-      </Grid>
+      <SkillsPack />
     </Section>
   );
 };
+// export default () => {
+//   return (
+//     <Section>
+//       <Grid container spacing={2}>
+//         <Grid item xs={6}>
+//           <SkillsPack />
+//         </Grid>
+//         <Grid item xs={6} sx={{ display: "flex", alignItems: "center" }}>
+//           <Box>
+//             <Typography variant="h2" fontWeight="bold" mb={2}>
+//               Huge pack of Skills
+//             </Typography>
+
+//             <Button component={Link} href="/skills" variant="contained">
+//               Show All Skills
+//             </Button>
+//           </Box>
+//         </Grid>
+//       </Grid>
+//     </Section>
+//   );
+// };
