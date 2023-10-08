b7d2867779db8917f065ee6b58425abf54a3bb9e
thoriqdharmawan <thoriqd21@gmail.com>
2023-10-08 17:45:17 +0700

    feat: add buble ui

--- components/shared/BubbleSkils.jsx
+++ components/shared/BubbleSkils.jsx
@@ -1,85 +1,85 @@
-// import { Box, Paper, Tooltip, Typography } from "@mui/material";
-// // import BubbleUI from "react-bubble-ui";
+import { Box, Paper, Tooltip, Typography } from "@mui/material";
+import BubbleUI from "react-bubble-ui";
 
-// import { SKILLS } from "@/constant/global";
+import { SKILLS } from "@/constant/global";
 
-// // import "react-bubble-ui/dist/index.css";
-// import "./bubble.css";
+import "react-bubble-ui/dist/index.css";
+import "./bubble.css";
 
-// const Child = ({ name, src, bgColor }) => {
-//   return (
-//     <Box
-//       sx={{
-//         ...styles.bubble,
-//         backgroundColor: bgColor,
-//       }}
-//       className="bubbleUI-childComponent"
-//     >
-//       <Paper elevation={0} sx={styles.skillIcon}>
-//         <img src={src} width={50} height={50} alt={name} />
-//       </Paper>
-//       <Typography sx={styles.bubbleName}>{name}</Typography>
-//     </Box>
-//   );
-// };
+const Child = ({ name, src, bgColor }) => {
+  return (
+    <Box
+      sx={{
+        ...styles.bubble,
+        backgroundColor: bgColor,
+      }}
+      className="bubbleUI-childComponent"
+    >
+      <Paper elevation={0} sx={styles.skillIcon}>
+        <img src={src} width={50} height={50} alt={name} />
+      </Paper>
+      <Typography sx={styles.bubbleName}>{name}</Typography>
+    </Box>
+  );
+};
 
-// const options = {
-//   size: 152,
-//   minSize: 14,
-//   gutter: 7,
-//   provideProps: true,
-//   numCols: 3,
-//   fringeWidth: 120,
-//   yRadius: 103,
-//   xRadius: 150,
-//   cornerRadius: 0,
-//   showGuides: false,
-//   compact: true,
-//   gravitation: 0,
-// };
+const options = {
+  size: 152,
+  minSize: 14,
+  gutter: 7,
+  provideProps: true,
+  numCols: 3,
+  fringeWidth: 120,
+  yRadius: 103,
+  xRadius: 150,
+  cornerRadius: 0,
+  showGuides: false,
+  compact: true,
+  gravitation: 0,
+};
 
-// const BubbleSkills = () => {
-//   const children = SKILLS.slice(0, 10)?.map((data, idx) => (
-//     <Child key={idx} name={data?.name} src={data.src} bgColor={data.bgColor} />
-//   ));
+const BubbleSkills = () => {
+  const children = SKILLS.slice(0, 10)?.map((data, idx) => (
+    <Child key={idx} name={data?.name} src={data.src} bgColor={data.bgColor} />
+  ));
 
-//   return (
-//     <Box sx={{ height: "400px", width: "100%" }}>
-//       <BubbleUI options={options} className="bubbleUI-root">
-//         {children}
-//       </BubbleUI>
-//     </Box>
-//   );
-// };
+  return (
+    <Box sx={{ height: "400px", width: "100%" }}>
+      <BubbleUI options={options} className="bubbleUI-root">
+        {children}
+      </BubbleUI>
+    </Box>
+  );
+};
 
-// export default BubbleSkills;
+export default BubbleSkills;
 
-// const styles = {
-//   skillIcon: {
-//     backgroundColor: "unset",
-//     width: "fit-content",
-//     padding: "8px",
-//     display: "flex",
-//     alignItems: "center",
-//     justifyContent: "center",
-//     borderRadius: "8px",
-//     position: "relative",
-//     "&:hover": {
-//       boxShadow: "rgba(145, 158, 171, 0.16) 0px 24px 48px 0px",
-//     },
-//   },
-//   bubble: {
-//     borderRadius: "50%",
-//     width: "100%",
-//     height: "100%",
-//     display: "flex",
-//     flexDirection: "column",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-//   bubbleName: {
-//     color: "#fff",
-//     fontWeight: "600",
-//     mt: "8px",
-//   },
-// };
+const styles = {
+  skillIcon: {
+    backgroundColor: "unset",
+    width: "fit-content",
+    padding: "8px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: "8px",
+    position: "relative",
+    "&:hover": {
+      boxShadow: "rgba(145, 158, 171, 0.16) 0px 24px 48px 0px",
+    },
+  },
+  bubble: {
+    borderRadius: "50%",
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bubbleName: {
+    color: "#fff",
+    fontWeight: "600",
+    mt: "8px",
+  },
+};
--- components/ui/Skills.jsx
+++ components/ui/Skills.jsx
@@ -2,14 +2,14 @@ import { Box, Button, Grid, Typography } from "@mui/material";
 import Link from "next/link";
 import Section from "../shared/Section";
 import SkillsPack from "../shared/SkillsPack";
-// import BubbleSkills from "../shared/BubbleSkils";
+import BubbleSkills from "../shared/BubbleSkils";
 
 export default () => {
   return (
     <Section py={2}>
       <Grid container spacing={2} sx={styles.gridContainer}>
         <Grid item lg={6} sx={styles.gridItem}>
-          {/* <BubbleSkills /> */}
+          <BubbleSkills />
         </Grid>
         <Grid item md={12} lg={6} sx={styles.gridItem}>
           <Box sx={{ width: "100%" }}>
