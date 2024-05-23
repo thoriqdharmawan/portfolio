656784d2c6017ba7008ce7714ac88185adb83db3
thoriqdharmawan <thoriqd21@gmail.com>
2024-05-23 22:24:04 +0700

    fix(header): now showing header when go back to homepage

--- app/page.js
+++ app/page.js
@@ -1,32 +1,36 @@
 "use client";
 
 import Content from "@/components/shared/Content";
-import Libraries from "@/components/ui/Libraries";
+// import Libraries from "@/components/ui/Libraries";
 import Hero from "@/components/ui/Hero";
 import Projects from "@/components/ui/Projects";
 // import Skills from "@/components/ui/Skills";
-import WhatCanBeDone from "@/components/ui/WhatCanBeDone";
+// import WhatCanBeDone from "@/components/ui/WhatCanBeDone";
 import Testimonials from "@/components/ui/Testimonials";
-import Me from "@/components/ui/Me";
+// import Me from "@/components/ui/Me";
+import Header from "@/components/shared/Header";
 
 export default function Home() {
   return (
-    <main>
-      <Hero />
+    <>
+      <Header />
+      <main>
+        <Hero />
 
-      <Content>
-        {/* <Me /> */}
+        <Content>
+          {/* <Me /> */}
 
-        {/* <WhatCanBeDone /> */}
+          {/* <WhatCanBeDone /> */}
 
-        {/* <Skills /> */}
+          {/* <Skills /> */}
 
-        <Projects />
+          <Projects />
 
-        {/* <Libraries /> */}
+          {/* <Libraries /> */}
 
-        <Testimonials />
-      </Content>
-    </main>
+          <Testimonials />
+        </Content>
+      </main>
+    </>
   );
 }
